f5bbf542aa4c14eb887ace1d17bf01e23f74a46c
Joana <asiakom24@vp.pl>
2025-09-12 17:00:56 +0200

    stream plot

--- stream_plot.py
+++ stream_plot.py
@@ -37,12 +37,13 @@ for chunk_id in range(400):
     clf.partial_fit(X,y,[0,1])    
 
 # Plot concept clusters
-fig, ax = plt.subplots(3,1,figsize=(10,5))
+fig, ax = plt.subplots(3,1,figsize=(10,5), 
+                       height_ratios=[7,3,2])
 
 cols = plt.cm.coolwarm(np.linspace(0,1,4))
 
 ax[1].plot(accs, c='black')
-ax[1].set_xticks(drifts_moments)
+ax[1].set_xticks(drifts_moments, ['','',''])
 # ax[2].scatter(np.arange(400), clusters, c=cols[clusters])
 ax[2].scatter(np.arange(400), drifts_gt, c=cols[drifts_gt])
 ax[2].set_xticks(drifts_moments)
@@ -54,7 +55,7 @@ ax[1].set_xlim(0,400)
 ax[2].set_xlim(0,400)
 
 ax[1].set_ylabel('accuracy')
-ax[2].set_ylabel('concept identifier')
+ax[2].set_ylabel('concept\nground-truth')
 # ax[1].set_xlabel('chunk of a concept change')
 ax[2].set_xlabel('chunk index / concept change')
 
@@ -65,7 +66,7 @@ ax[0].spines['right'].set_visible(False)
 ax[0].spines['bottom'].set_visible(False)
 ax[0].spines['left'].set_visible(False)
 
-ax1 = plt.subplot(3,4,1)
+ax1 = plt.subplot(2,4,1)
 # pca1 = PCA(n_components=2).fit_transform(chunks_dist[0][0])
 pca1 = chunks_dist[0][0]
 ax1.scatter(pca1[:,0], pca1[:,1], c=chunks_dist[0][1], cmap='coolwarm', s=10)
@@ -74,7 +75,7 @@ ax1.spines['right'].set_visible(False)
 ax1.grid(ls=':')
 ax1.set_title('chunk: %i' % chunks_idx[0])
 
-ax2 = plt.subplot(3,4,2)
+ax2 = plt.subplot(2,4,2)
 # pca2 = PCA(n_components=2).fit_transform(chunks_dist[1][0])
 pca2 = chunks_dist[1][0]
 ax2.scatter(pca2[:,0], pca2[:,1], c=chunks_dist[1][1], cmap='coolwarm', s=10)
@@ -83,7 +84,7 @@ ax2.spines['right'].set_visible(False)
 ax2.grid(ls=':')
 ax2.set_title('chunk: %i' % chunks_idx[1])
 
-ax3 = plt.subplot(3,4,3)
+ax3 = plt.subplot(2,4,3)
 # pca3 = PCA(n_components=2).fit_transform(chunks_dist[2][0])
 pca3 = chunks_dist[2][0]
 ax3.scatter(pca3[:,0], pca3[:,1], c=chunks_dist[2][1], cmap='coolwarm', s=10)
@@ -92,7 +93,7 @@ ax3.spines['right'].set_visible(False)
 ax3.grid(ls=':')
 ax3.set_title('chunk: %i' % chunks_idx[2])
 
-ax4 = plt.subplot(3,4,4)
+ax4 = plt.subplot(2,4,4)
 # pca4 = PCA(n_components=2).fit_transform(chunks_dist[3][0])
 pca4 = chunks_dist[3][0]
 ax4.scatter(pca4[:,0], pca4[:,1], c=chunks_dist[3][1], cmap='coolwarm', s=10)
@@ -106,6 +107,15 @@ for aa in [ax1, ax2, ax3, ax4]:
     aa.set_ylabel('feature 1')
     
 ax[2].set_yticks([0,1,2,3])
+
+ax[1].spines['top'].set_visible(False)
+ax[1].spines['right'].set_visible(False)
+
+ax[2].spines['top'].set_visible(False)
+ax[2].spines['right'].set_visible(False)
+
+fig.align_ylabels()
+
 
 plt.tight_layout()
 plt.savefig('foo.png')
