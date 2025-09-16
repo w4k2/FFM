scaling results fixed

E_R1_scaling_vis3_ref.py
@@ -15,6 +15,13 @@ dims = np.linspace(8, 512, 10).astype(int)
 res = np.load('res/e_r1_scale.npy')*1000
 res_ref = np.load('res/e_r1_scale_pca.npy')*1000
 
+# print(res_ref[:,-1,0,-1])
+# mask = res_ref[:,-1,0,-1]>150
+# res_ref[mask,-1,0,-1] = np.nan
+# print(res_ref[:,-1,0,-1])
+
+# exit()
+
 print(res.shape) # (10, 6, 6, 8) = reps, chunks, chunk size, dims
 mean_res = np.nanmean(res, axis=0)
 mean_res_ref = np.nanmean(res_ref, axis=0)
@@ -22,19 +29,23 @@ mean_res_ref = np.nanmean(res_ref, axis=0)
 fig, ax = plt.subplots(1,3,figsize=(12,4), sharex=False, sharey=True)
 cols = plt.cm.coolwarm(np.linspace(0,1,len(chunk_size)))
 
-chs_id = -1
-chs = chunk_size[chs_id]
-nch_id = -1
-nch = n_chunks[nch_id]
+# ids = [-1,-2,-3]
+ids = [-1,-2]
 
-ax[0].plot(mean_res[:,chs_id,-1], 
-               label='FFM | chunk size = %i' % chs,
-               color=cols[chs_id])
-ax[0].plot(mean_res_ref[:,chs_id,-1], 
-                label='PCA | chunk size = %i' % chs,
-               color=cols[chs_id], ls=':')
-ax[0].scatter(np.arange(len(n_chunks)), mean_res[:,chs_id,-1], 
-               color=cols[chs_id], marker="$\u25EF$")
+for id in ids:
+    chs = chunk_size[id]
+    nch = n_chunks[id]
+
+    ax[0].plot(mean_res[:,id,-1], 
+                label='FFM | chunk size = %i' % chs,
+                color=cols[id])
+    ax[0].plot(mean_res_ref[:,id,-1], 
+                    label='PCA | chunk size = %i' % chs,
+                color=cols[id], ls=':')
+    ax[0].scatter(np.arange(len(n_chunks)), mean_res[:,id,-1], 
+                color=cols[id], marker="$\u25EF$")
+    ax[0].scatter(np.arange(len(n_chunks)), mean_res_ref[:,id,-1], 
+                color=cols[id], marker="$\u25EF$")
     
 ax[0].set_xlabel('number of chunks')
 ax[0].set_xticks(np.arange(len(n_chunks)), n_chunks)
@@ -42,14 +53,20 @@ ax[0].set_ylabel('time [ms]')
 ax[0].legend(frameon=False)
 
 cols = plt.cm.coolwarm(np.linspace(0,1,len(n_chunks)))
-ax[1].plot(mean_res[nch_id,:,-1], 
-            label='FFM | n chunks = %i' % nch,
-            color=cols[nch_id])
-ax[1].scatter(np.arange(len(chunk_size)), mean_res[nch_id,:,-1], 
-            color=cols[nch_id], marker="$\u25EF$")
-ax[1].plot(mean_res_ref[nch_id,:,-1], 
-            label='PCA | n chunks = %i' % nch, 
-            color=cols[nch_id], ls=':')
+
+for id in ids:
+    chs = chunk_size[id]
+    nch = n_chunks[id]
+    ax[1].plot(mean_res[id,:,-1], 
+                label='FFM | n chunks = %i' % nch,
+                color=cols[id])
+    ax[1].scatter(np.arange(len(chunk_size)), mean_res[id,:,-1], 
+                color=cols[id], marker="$\u25EF$")
+    ax[1].plot(mean_res_ref[id,:,-1], 
+                label='PCA | n chunks = %i' % nch, 
+                color=cols[id], ls=':')
+    ax[1].scatter(np.arange(len(chunk_size)), mean_res_ref[id,:,-1], 
+                color=cols[id], marker="$\u25EF$")
 
 ax[1].set_xlabel('chunk size')
 ax[1].set_xticks(np.arange(len(chunk_size)), chunk_size)
@@ -57,15 +74,20 @@ ax[1].legend(frameon=False)
 # ax[1].set_ylabel('time')
 
 cols = plt.cm.coolwarm(np.linspace(0,1,len(n_chunks)))
-ax[2].plot(mean_res[nch_id,-1,], 
-            label='FFM | n chunks = %i' % nch,
-            color=cols[nch_id])
-ax[2].plot(mean_res_ref[nch_id,-1,],
-            label='PCA | n chunks = %i' % nch, 
-            color=cols[nch_id], ls=':')
-ax[2].scatter(np.arange(len(dims)), mean_res[nch_id,-1,:], 
-            color=cols[nch_id], marker="$\u25EF$")
-    
+for id in ids:
+    chs = chunk_size[id]
+    nch = n_chunks[id]
+    ax[2].plot(mean_res[id,-1,], 
+                label='FFM | n chunks = %i' % nch,
+                color=cols[id])
+    ax[2].plot(mean_res_ref[id,-1,],
+                label='PCA | n chunks = %i' % nch, 
+                color=cols[id], ls=':')
+    ax[2].scatter(np.arange(len(dims)), mean_res[id,-1,:], 
+                color=cols[id], marker="$\u25EF$")
+    ax[2].scatter(np.arange(len(dims)), mean_res_ref[id,-1,:], 
+                color=cols[id], marker="$\u25EF$")
+
 ax[2].set_xlabel('data dimensionality')
 ax[2].set_xticks(np.arange(len(dims)), dims)
 ax[2].legend(frameon=False)